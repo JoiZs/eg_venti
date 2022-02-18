6dea6ce8a5447c9ecfe37f8ebb80c2f4697287e5
SeeeSaww <pyaesoneaung1126@hotmail.com>
2022-02-18 08:58:47 +0630

    morning_modi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-restricted-globals */
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import gsap from "gsap";
+import BeatLoader from "react-spinners/BeatLoader";
 
 function App() {
+  const [modelLoading, setModelLoading] = useState(false);
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
     72,
@@ -33,42 +35,46 @@ function App() {
     );
   });
 
-  loader.load(
-    "./gltf/ventilator2.gltf",
-    (gltf) => {
-      const model = gltf.scene;
-      model.position.set(0, 0, 0);
-      model.scale.set(0.003, 0.003, 0.003);
-      model.rotation.set(0.18, -3.5, 0.13);
-      scene.add(model);
-      const mixer = new THREE.AnimationMixer(model);
-      const clips = gltf.animations;
+  const load3d = async () => {
+    let isloaded = await loader.loadAsync("./gltf/ventilator2.gltf");
+    !isloaded ? setModelLoading(false) : setModelLoading(true);
+    loader.load(
+      "./gltf/ventilator2.gltf",
+      (gltf) => {
+        const model = gltf.scene;
+        model.position.set(0, 0, 0);
+        model.scale.set(0.003, 0.003, 0.003);
+        model.rotation.set(0.18, -3.5, 0.13);
+        scene.add(model);
+        const mixer = new THREE.AnimationMixer(model);
+        const clips = gltf.animations;
 
-      for (let i = 0; i < clips.length; i++) {
-        const action = mixer.clipAction(clips[i]);
-        action.play();
+        for (let i = 0; i < clips.length; i++) {
+          const action = mixer.clipAction(clips[i]);
+          action.play();
+        }
+
+        const clock = new THREE.Clock();
+        function animate() {
+          mixer.update(clock.getDelta());
+          requestAnimationFrame(animate);
+          controls.update();
+
+          model.rotation.y += 0.003;
+          renderer.render(scene, camera);
+        }
+
+        animate();
+      },
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100);
+      },
+      function (error) {
+        console.log("An error happened");
       }
-
-      const clock = new THREE.Clock();
-      function animate() {
-        mixer.update(clock.getDelta());
-        requestAnimationFrame(animate);
-        controls.update();
-
-        model.rotation.y += 0.003;
-        renderer.render(scene, camera);
-      }
-
-      animate();
-    },
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    function (error) {
-      console.log("An error happened");
-    }
-  );
-
+    );
+  };
+  load3d();
   const light = new THREE.AmbientLight(0xffffff, 0.6); // soft white light
   scene.add(light);
 
@@ -82,13 +88,24 @@ function App() {
 
   return (
     <div className="App">
-      <div className="text-slate-700 tracking-wider md:text-6xl text-4xl uppercase font-bold absolute top-3/4 text-center -translate-x-1/2 md:top-1/2  left-1/2">
-        <h1 id="title"> ventilation device</h1>
-        <button className="border border-slate-600 p-2 rounded uppercase text-2xl hover:bg-slate-100 ">
-          Contact Now
-        </button>
-      </div>
-      <div className="canvasDiv bg-slate-200"></div>
+      {!modelLoading ? (
+        <div className="flex justify-center items-center w-screen h-screen bg-slate-300">
+          <BeatLoader />
+        </div>
+      ) : (
+        <>
+          <div className="text-slate-500  tracking-wider md:text-6xl text-4xl uppercase font-bold absolute top-3/4 text-center -translate-x-1/2 md:top-1/2  left-1/2">
+            <h1 id="title " className="invert">
+              {" "}
+              ventilation device
+            </h1>
+            <button className="border border-slate-600 p-2 rounded uppercase text-2xl hover:bg-slate-100 ">
+              Contact Now
+            </button>
+          </div>
+          <div className="canvasDiv bg-slate-200"></div>
+        </>
+      )}
     </div>
   );
 }
